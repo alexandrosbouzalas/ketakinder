bc715bbb93c8bd02aff53f51fdf60ba085c91368
HUSTENSATSCHMUGGLER <florian.saathoff@gmx.com>
2022-05-13 09:20:06 +0200

    Single user Player Template

--- public/js/w2g/w2g.js
+++ public/js/w2g/w2g.js
@@ -2,3 +2,43 @@ document.getElementById("first").href = "/";
 document.getElementById("first").innerText = "Home";
 document.getElementById("second").href = "/logout";
 document.getElementById("second").innerText = "Logout";
+
+
+function validateInputString(input) {
+
+    const reInputStringShort = /^https:\/\/youtube\.com\/watch\?v=[a-zA-Z0-9_]+$/;
+    const reInputStringFull = /^https:\/\/www\.youtube\.com\/watch\?v=[a-zA-Z0-9_]+$/;
+
+    if (!reInputStringShort.test(input) && !reInputStringFull.test(input) ) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+$('#platform-btn').click(() => {
+    window.location.href = "https://youtube.com" 
+})
+
+$('#search-btn').click(() => {
+
+    let inputString = $('#video-input').val();
+    let linkTemplate = "https://www.youtube.com/embed/" 
+
+    if(inputString) {
+
+        if(validateInputString(inputString)) {
+
+            inputString = inputString.split('=');
+            $('#video-player').attr('src', linkTemplate + inputString[inputString.length - 1]);
+            $("#video-input").css("border", "none");
+            $("#video-player-text").css("display", "none");
+            $("#video-player").css("display", "block");
+            
+        } else {
+            $("#video-input").css("border", "3px solid red");
+        }
+            
+    }
+    
+})
